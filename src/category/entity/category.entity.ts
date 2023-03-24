67ac1d8df1248f13371933e35cb56f6f57646b10
stizzle123 <arifayanidowu2@yahoo.com>
2023-03-24 13:27:41 +0100

    Updated category.entity

--- src/category/entity/category.entity.ts
+++ src/category/entity/category.entity.ts
@@ -1,13 +1,20 @@
+import { Category } from '@prisma/client';
 import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
-export class CategoryEntity {
-  id: string;
+export class CategoryEntity implements Category {
+  id: Category['id'];
 
   @IsString()
   @IsNotEmpty()
   @Transform((params) => params.value.trim()?.toLowerCase())
-  name: string;
+  name: Category['name'];
+
+  @IsDate()
+  createdAt: Category['createdAt'] = new Date();
+
+  @IsDate()
+  updatedAt: Category['updatedAt'] = new Date();
 
   constructor(partial: Partial<CategoryEntity>) {
     Object.assign(this, partial);
